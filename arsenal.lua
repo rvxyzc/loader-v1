7360826a7dcc1116ae2164c8261af27230789eb9
rvxyzc <putninsg7@gmail.com>
2025-04-18 21:14:27 +0100

    Update arsenal.lua

--- arsenal.lua
+++ arsenal.lua
@@ -1,1 +1,23 @@
+--[[
+.____                     .___             ____   ________ 
+|    |    _________     __| _/___________  \   \ /   /_   |
+|    |   /  _ \__  \   / __ |/ __ \_  __ \  \   Y   / |   |
+|    |__(  <_> ) __ \_/ /_/ \  ___/|  | \/   \     /  |   |
+|_______ \____(____  /\____ |\___  >__|       \___/   |___|
+        \/         \/      \/    \/                        
+--]]
 
+local StarterGui = game:GetService("StarterGui")
+local bindable = Instance.new("BindableFunction")
+
+function bindable.OnInvoke(response)
+    print("LOADER V1")
+end
+
+StarterGui:SetCore("SendNotification", {
+	Title = "Notification",
+	Text = "Script loaded succesfully",
+	Duration = 500,
+	Callback = bindable,
+	Button1 = "Close",
+})

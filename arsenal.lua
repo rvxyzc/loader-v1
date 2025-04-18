200e508fea9b532ff2bda08c66c9e4d9d3c54cdb
rvxyzc <putninsg7@gmail.com>
2025-04-18 22:19:15 +0100

    Update arsenal.lua

--- arsenal.lua
+++ arsenal.lua
@@ -2,13 +2,34 @@ local StarterGui = game:GetService("StarterGui")
 local bindable = Instance.new("BindableFunction")
 
 function bindable.OnInvoke(response)
-    print("LOADER V1")
+    print("")
 end
 
 StarterGui:SetCore("SendNotification", {
-	Title = "Notification",
+	Title = "Seb's Hub",
 	Text = "Script loaded succesfully",
 	Duration = 500,
 	Callback = bindable,
 	Button1 = "Close",
+})
+
+local Bracket = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR32/Bracket/main/BracketV32.lua"))()
+local Window = Bracket:Window({
+    Name = "Seb's Hub",
+    Enabled = true,
+    Color = Color3.new(0.2, 0.6, 1),
+    Size = UDim2.new(0, 600, 0, 450),
+    Position = UDim2.new(0.5, -300, 0.5, -225)
+})
+
+local Tab = Window:Tab({Name = "Main"})
+Tab:Divider({Text = "Combat", Side = "Left"})
+
+local Toggle = Tab:Toggle({
+    Name = "Aimbot",
+    Side = "Left",
+    Value = false,
+    Callback = function(Bool)
+        print("Feature enabled: ", Bool)
+    end
 })
